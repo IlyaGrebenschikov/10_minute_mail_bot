5d7fdddc0972b88f4347034839ef93abd5c276c9
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-19 00:50:36 +0400

    user schema

---
+++ src/database/schemas/user_schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import field_validator
+
+
+class AccountSchema(BaseModel):
+    address: EmailStr
+    password: str
+
+    @field_validator("password")
+    def check_password(cls, value):
+        value = str(value)
+        if len(value) < 8:
+            raise ValueError("Password must have at least 8 characters")
+        if not any(c.isupper() for c in value):
+            raise ValueError("Password must have at least one uppercase letter")
+        if not any(c.islower() for c in value):
+            raise ValueError("Password must have at least one lowercase letter")
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Password must have at least one digit")
+        return value
