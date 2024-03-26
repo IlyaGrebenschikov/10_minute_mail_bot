0499797949695cbccc8d0a79ee80617ea3aac265
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-26 06:37:19 +0400

    Abstract client changes

--- src/common/mail_client/base.py
+++ src/common/mail_client/base.py
@@ -2,42 +2,40 @@ from typing import Optional
 from abc import ABC
 from abc import abstractmethod
 
-from src.database.schemas import AccountSchema
-
 
 class AbstractClient(ABC):
     @abstractmethod
-    async def fetch_data_post(self, url: str, headers: Optional[dict], json: Optional[dict]):
+    async def fetch_data_post(self, **kwargs):
         pass
 
     @abstractmethod
-    async def fetch_data_get(self, url: str, headers: Optional[dict]):
+    async def fetch_data_get(self, **kwargs):
         pass
 
     @abstractmethod
-    def _domain_verification(self, data: dict):
+    def _domain_verification(self, **kwargs):
         pass
 
     @abstractmethod
-    async def _get_domain(self):
+    async def _get_domain(self, **kwargs):
         pass
 
     @abstractmethod
-    async def _create_user_data(self):
+    async def _create_user_data(self, **kwargs):
         pass
 
     @abstractmethod
-    async def create_account(self):
+    async def create_account(self, **kwargs):
         pass
 
     @abstractmethod
-    async def create_token(self):
+    async def create_token(self, **kwargs):
         pass
 
     @abstractmethod
-    async def get_messages_all(self):
+    async def get_messages_all(self, **kwargs):
         pass
 
     @abstractmethod
-    async def get_message_id(self, message_id: int):
+    async def get_message_id(self, **kwargs):
         pass
