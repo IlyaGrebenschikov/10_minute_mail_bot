dffec81d6098d3fa5d2c3b089e268851ca60eede
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-26 07:16:11 +0400

    refactor

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,24 +11,25 @@ from pydantic import DirectoryPath
 ROOT_DIR: DirectoryPath = Path(__file__).parent.parent.parent
 
 
-class BotSettings(BaseSettings):
+class EnvSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=f'{ROOT_DIR}/.env',
         env_file_encoding='utf-8',
     )
+
     TOKEN: str
-    PARSE_MODE: ParseMode | str = ParseMode.HTML
 
-
-class RedisSettings:
-    model_config = SettingsConfigDict(
-        env_file=f'{ROOT_DIR}/.env',
-        env_file_encoding='utf-8',
-    )
     REDIS_URL: str
     REDIS_PORT: str
     REDIS_HOST: str
 
+
+class BotSettings(EnvSettings):
+    TOKEN: str
+    PARSE_MODE: ParseMode | str = ParseMode.HTML
+
+
+class RedisSettings(EnvSettings):
     @property
     def get_url(self) -> str:
         return self.REDIS_URL.format(
