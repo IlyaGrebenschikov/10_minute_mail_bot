e5e7267f2d13e529aff2417cf5fded283b770811
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-26 08:24:49 +0400

    refactor and include start router

--- src/main.py
+++ src/main.py
@@ -6,18 +6,20 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.bot import DefaultBotProperties
 
 from src.core import get_settings
+from src.handlers.users import start_router
 
 
 async def main():
-    token = get_settings().bot.TOKEN
-    parse_mode = get_settings().bot.PARSE_MODE
-
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    bot = Bot(
+        get_settings().bot.TOKEN,
+        default=DefaultBotProperties(parse_mode=get_settings().bot.PARSE_MODE)
+    )
     dp = Dispatcher()
-    bot = Bot(token, default=DefaultBotProperties(parse_mode=parse_mode))
+    dp.include_router(start_router)
 
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
