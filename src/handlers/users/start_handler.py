a6bce12eeb27fc451666533190b99c6516acf7a9
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-27 09:21:54 +0400

    register help_kb in start handler

--- src/handlers/users/start_handler.py
+++ src/handlers/users/start_handler.py
@@ -3,12 +3,16 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
+from src.keyboards import help_keyboard
+
 
 start_router = Router()
 
 
 @start_router.message(CommandStart())
 async def start_message(message: Message):
+    help_kb = help_keyboard()
     await message.answer(
-        f'Добрый день, {hbold(message.from_user.full_name)}!'
+        f'Добрый день, {hbold(message.from_user.full_name)}!',
+        reply_markup=help_kb,   # todo Создать keyboard bilder, чтобы регать все кнопки
     )
