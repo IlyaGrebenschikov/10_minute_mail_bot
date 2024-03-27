0e62d22d33869946af6574242df472cbe0f592a7
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-27 09:19:35 +0400

    help handler

---
+++ src/handlers/users/help_handler.py
@@ -0,0 +1,20 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+from src.keyboards import help_keyboard
+
+
+help_router = Router()
+
+
+@help_router.message(Command('help'))
+async def start_message(message: Message):
+    keyboard = help_keyboard()
+    await message.answer(
+        'This is 10 minute mail bot.\n'
+        'This bot allows you to create an account and receive messages.\n'
+        f'Bot lifespan is {hbold("10 minutes")}.',
+        reply_markup=keyboard,
+    )
