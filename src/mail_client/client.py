922bdacbee0b5f919e0130503c5485ac9fd80288
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-27 08:25:31 +0400

    messages methods for client

--- src/mail_client/client.py
+++ src/mail_client/client.py
@@ -89,14 +89,18 @@ class MailClient(AbstractClient):
         link = f'{self.base_url}token'
         data = self.user_data
         request = await self.fetch_data_post(link, json=data)
-        self.user_token = {"Authorization": f'Bearer {request.get("token")}'}
+        self.user_token = {"Authorization": f'Bearer {request.get("token")}'}   # todo переписать под редис, в репозитории
         return request
 
-    async def get_messages_all(self):
-        pass
+    async def get_messages_all(self, token: dict, page: int = 1) -> dict:
+        link = f'{self.base_url}messages?page={page}'
+        request = await self.fetch_data_get(link, token)
+        return request
 
-    async def get_message_id(self, message_id: int):
-        pass
+    async def get_message_id(self, token: dict, message_id: str) -> dict:
+        link = f'{self.base_url}messages/{message_id}'
+        request = await self.fetch_data_get(link, token)
+        return request
 
 
 async def main():
@@ -105,10 +109,22 @@ async def main():
     data = client.user_data
     token = await client.create_token()
     token_data = client.user_token
-    pprint(user)
-    # pprint(data)
-    # pprint(token_data)
-    pprint(token)
+    messages = await client.get_messages_all(token_data)
+    # pprint(user)
+    pprint(data)
+    print(token_data)
+    # pprint(token)
+    pprint(messages)
+
+
+async def second():
+    client = MailClient()
+    token = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MTE1MTMxMzUsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6InF0aXBncm91eHFAbWF4YW1iYS5jb20iLCJpZCI6IjY2MDM5ZTJmMDkzNGUxMDJmMjE2MWFhNiIsIm1lcmN1cmUiOnsic3Vic2NyaWJlIjpbIi9hY2NvdW50cy82NjAzOWUyZjA5MzRlMTAyZjIxNjFhYTYiXX19.whdJSa5tZeJkZ7bEX2wuhJA_Uc4wdbriVu_Kv1Iw4swDR8KD4T2yerlJr4QotIfMgTEVCUGuBrQZgGix4mA4Kg'}
+    messages = await client.get_messages_all(token)
+    message_id = await client.get_message_id(token, '66039e515658baa442b4e27e')
+    pprint(messages)
+    pprint(message_id)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    # asyncio.run(main())
+    asyncio.run(second())
