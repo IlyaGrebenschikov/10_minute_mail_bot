609d21b0d784f94a0deda3a396b72cdc17620021
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-28 15:19:28 +0400

    mail router

--- src/handlers/users/routers.py
+++ src/handlers/users/routers.py
@@ -2,8 +2,10 @@ from aiogram import Router
 
 from src.handlers.users.start_handler import start_router
 from src.handlers.users.help_handler import help_router
+from src.handlers.users.mail_handlers import mail_router
 
 
 users_router = Router()
 users_router.include_router(start_router)
 users_router.include_router(help_router)
+users_router.include_router(mail_router)
