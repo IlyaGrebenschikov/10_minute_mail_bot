beb394f25a9243ff41c83052acef6987be83dc25
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-04-14 15:32:16 +0400

    refactor session attr

--- src/mail_client/client.py
+++ src/mail_client/client.py
@@ -7,6 +7,8 @@ from random import choice
 
 import asyncio
 
+import aiohttp
+
 from src.mail_client.client_session import (
     ClientSession,
     fetch_data_get,
@@ -18,8 +20,13 @@ class MailClient:
     """
     docs https://docs.mail.gw/
     """
-    def __init__(self):
+    def __init__(
+            self,
+            headers: Optional[dict] = None,
+            auth: Optional[dict] = None,
+    ) -> None:
         self.base_url = 'https://api.mail.gw/'
+        self.__session = ClientSession(headers, auth)
 
     def __domain_verification(self, data: dict) -> str:
         for elems in data['hydra:member']:
@@ -27,10 +34,9 @@ class MailClient:
                 return elems.get('domain')
 
     async def __get_domain(self) -> str:
-        client = ClientSession()
         for page in range(1, 11):
             link = f'{self.base_url}domains?page={page}'
-            data = await fetch_data_get(client, link)
+            data = await fetch_data_get(self.__session, link)
             verification = self.__domain_verification(data)
 
             if verification:
@@ -55,29 +61,25 @@ class MailClient:
         return data
 
     async def create_account(self) -> Optional[dict]:
-        client = ClientSession()
         link = f'{self.base_url}accounts'
         data = await self.__create_user_data()
-        await fetch_data_post(client, link, json=data)
+        await fetch_data_post(self.__session, link, json=data)
         return data
 
     async def create_token(self, user_data: dict[str, str]) -> Optional[dict]:
-        client = ClientSession()
         link = f'{self.base_url}token'
-        request = await fetch_data_post(client, link, json=user_data)
+        request = await fetch_data_post(self.__session, link, json=user_data)
         token = request.get('token')
         return {'Authorization': f'Bearer {token}'}
 
-    async def get_messages_all(self, token: dict, page: int = 1) -> dict:
-        client = ClientSession(token)
+    async def get_messages_all(self, page: int = 1) -> dict:
         link = f'{self.base_url}messages?page={page}'
-        request = await fetch_data_get(client, link)
+        request = await fetch_data_get(self.__session, link)
         return request
 
-    async def get_message_id(self, token: dict, message_id: str) -> dict:
-        client = ClientSession(token)
+    async def get_message_id(self, message_id: str) -> dict:
         link = f'{self.base_url}messages/{message_id}'
-        request = await fetch_data_get(client, link)
+        request = await fetch_data_get(self.__session, link)
         return request
 
 
@@ -87,13 +89,13 @@ async def main():
     print(account)
     token = await client.create_token(account)
     print(token)
+    client = MailClient(token)
     while True:
-        message = await client.get_messages_all(token)
+        message = await client.get_messages_all()
         print(message)
         time.sleep(30)
         continue
 
 
 if __name__ == '__main__':
-    # asyncio.run(main())
     asyncio.run(main())
