fadf98cd452b7222e2338b1395d0badef87fd1b9
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-04-15 20:02:53 +0400

    dependency_provider

--- src/main.py
+++ src/main.py
@@ -2,15 +2,20 @@ import asyncio
 import logging
 import sys
 
+from fast_depends import dependency_provider
 from aiogram import Bot, Dispatcher
 from aiogram.client.bot import DefaultBotProperties
 
+
+from database.redis.redis_connect import redis_get_session
+from database.markers.redis import redis_marker
 from core.settings import get_settings
 from handlers.users.routers import users_router
 
 
 async def main():
     settings = get_settings()
+    dependency_provider.override(redis_marker, redis_get_session)
     bot = Bot(
         settings.bot.TOKEN.get_secret_value(),
         default=DefaultBotProperties(parse_mode=settings.bot.PARSE_MODE)
